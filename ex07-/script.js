d1bd564fbacf57ce3e02967cb7dc09ff7fe5493e
EddyPERREUL <e.perreul@gmail.com>
2020-07-15 12:38:09 +0200

    ex 07 and 09 finish

--- ex07-/script.js
+++ ex07-/script.js
@@ -1,5 +1,23 @@
 "use strict"
 const question = "Quel est le nom de ton école ?"
-let answer
+let answer = "Alyra"
 // pose la question jusqu'au moment ou l'utilisateur entre Alyra (la reponse est 'Alyra')
 // exprime ton enthousiasme
+
+//méthode plus appropriée
+ 
+do {
+ answer = prompt(question)
+} while (answer !== "Alyra")
+
+alert("Bravo! jeune padawan")
+
+
+// autre méthode, la boucle n'est pas générée si answer est spécifié
+/*
+while (answer !== "Alyra") {
+    answer = prompt(question)
+}
+
+alert("Bravo! jeune padawan")
+*/
--- ex09-/script.js
+++ ex09-/script.js
@@ -1,4 +1,23 @@
 "use strict"
 const start = 2
-const num = 10
-/* affiche les nombres de start au num, essaye while, do while et for */
+const end = 10
+/* affiche les nombres de start au end, essaye while, do while et for */
+
+/*
+let step = start
+while (step <= end) {
+    console.log(step)
+    step++
+}
+*/
+/*
+let step = start
+do {
+    console.log(step)
+    step += 1
+} while (step <= end)
+*/
+
+for (let step = start; step <= end; step += 1) {
+    console.log(step)
+}
